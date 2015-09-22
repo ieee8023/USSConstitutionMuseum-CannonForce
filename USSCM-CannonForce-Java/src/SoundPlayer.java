17a97b8da34fb1a0102a23537bf0062db979a2d2
ieee8023 <joseph@josephpcohen.com>
2015-09-22 14:09:58 -0400

    sound

--- USSCM-CannonForce-Java/src/SoundPlayer.java
+++ USSCM-CannonForce-Java/src/SoundPlayer.java
@@ -6,46 +6,51 @@ import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.UnsupportedAudioFileException;
 
 public class SoundPlayer {
 
-	public static void main(String[] args) throws UnsupportedAudioFileException, IOException {
+	public static void main(String[] args) {
 		
 		System.out.println(Arrays.toString(AudioSystem.getAudioFileTypes()));
 		
-		AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File("res/sound/boom.mp3"));
-		AudioFormat baseFormat = audioInputStream.getFormat();
-		
-		System.out.println(baseFormat);
+		playSound("res/sound/boom.mp3", true);
+
+        System.out.println("Done");
 		
 	}
 	
-	public static void playSound(String file) {
+	public static void playSound(String file, boolean block) {
 	    try {
 	    	
-//	    	AudioSystem.getAudioInputStream(new File(textField.getText()));
-//			AudioFormat baseFormat = audioInputStream.getFormat();
-//	    	
-//	    	AudioFormat decodedFormat = new AudioFormat(AudioFormat.Encoding.PCM_SIGNED,
-//					baseFormat.getSampleRate(),
-//					16,
-//					baseFormat.getChannels(),
-//					baseFormat.getChannels() * 2,
-//					baseFormat.getSampleRate(),
-//					false);
-//			
-//			AudioInputStream dis = AudioSystem.getAudioInputStream(decodedFormat, audioInputStream);
-//			audioInputStream = dis;
-//			audioFormat = dis.getFormat();
+			AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File(file));
+			AudioFormat baseFormat = audioInputStream.getFormat();
+			
+			//System.out.println(baseFormat);
 	    	
-	    	
-	    	
-	    	
-	        AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File(file));
+			AudioFormat decodedFormat = new AudioFormat(AudioFormat.Encoding.PCM_SIGNED,
+					baseFormat.getSampleRate(),
+					16,
+					baseFormat.getChannels(),
+					baseFormat.getChannels() * 2,
+					baseFormat.getSampleRate(),
+					false);
+			
+			AudioInputStream dis = AudioSystem.getAudioInputStream(decodedFormat, audioInputStream);
+			audioInputStream = dis;
+			AudioFormat audioFormat = dis.getFormat();
+			//System.out.println("Converted to: " + audioFormat);
+			
 	        Clip clip = AudioSystem.getClip();
 	        clip.open(audioInputStream);
 	        clip.start();
+	    	
+	        if (block)
+	            while(clip.getFrameLength() >= clip.getFramePosition()+1){
+	            	Thread.sleep(10);
+	            }
+	        
 	    } catch(Exception ex) {
 	        System.out.println("Error with playing sound.");
 	        ex.printStackTrace();
